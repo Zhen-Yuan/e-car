40a7337f6050b6cdd7a7fd7d486aa500699bff50
Zhen-Yuan <stefanie.chiao@web.de>
2017-11-28 22:23:48 +0100

    added upload profile image method

--- app/src/main/java/com/example/denis/ecar/sharedPref/ProfileFragment.java
+++ app/src/main/java/com/example/denis/ecar/sharedPref/ProfileFragment.java
@@ -4,11 +4,14 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -21,6 +24,16 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.denis.ecar.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
@@ -31,6 +44,7 @@ import java.util.Date;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import static android.app.Activity.RESULT_OK;
+import static com.facebook.FacebookSdk.getApplicationContext;
 import static com.facebook.FacebookSdk.getCacheDir;
 
 
@@ -42,7 +56,12 @@ public class ProfileFragment extends Fragment {
     private View view;
     private Dialog dialog;
     private CircleImageView profileImage;
-    private String mCurrentPhotoPath;
+    //private String mCurrentPhotoPath;
+    private Uri filePath;
+
+    private FirebaseAuth firebaseAuth;
+    private StorageReference firebaseStorage;
+
 
 
     @Override
@@ -61,6 +80,13 @@ public class ProfileFragment extends Fragment {
 
 
     private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            firebaseStorage = FirebaseStorage.getInstance().getReference().child(user.getUid());
+        }
         profileImage = (CircleImageView)view.findViewById(R.id.civProfile);
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -76,10 +102,10 @@ public class ProfileFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK && requestCode == REQUEST_GALLERY) {
+        if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
             beginCrop(data.getData());
             dialog.dismiss();
-        } else if (resultCode == RESULT_OK && requestCode == REQUEST_CAMERA) {
+        } else if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
             data.getStringExtra(MediaStore.EXTRA_OUTPUT);
             Bundle bundle = data.getExtras();
             final Bitmap bmp = (Bitmap) bundle.get("data");
@@ -96,7 +122,6 @@ public class ProfileFragment extends Fragment {
         ImageButton gallery = (ImageButton)view.findViewById(R.id.imgBttnGallery);
         ImageButton camera = (ImageButton)view.findViewById(R.id.imgBttnCamera);
         ImageButton delete = (ImageButton)view.findViewById(R.id.imgBttnDeleteImage);
-        Button cancel = (Button)view.findViewById(R.id.bttnCancel);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setView(view);
@@ -145,6 +170,45 @@ public class ProfileFragment extends Fragment {
         dialog.show();
     }
 
+
+    private void uploadImage(Uri uri) {
+        if (uri != null) {
+            firebaseStorage.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getContext(), "upload erfolgreich", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+/*
+    private void downloadImage() {
+
+        File localFile = File.createTempFile("images", ".jpg");
+        firebaseStorage.getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        // Successfully downloaded data to local file
+                        // ...
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle failed download
+                // ...
+            }
+        });
+    }   */
+
+
     /*
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -163,6 +227,7 @@ public class ProfileFragment extends Fragment {
         return image;
     }   */
 
+
     private void beginCrop(Uri source) {
         Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
         Crop.of(source, destination).asSquare().start(getActivity());
@@ -170,7 +235,9 @@ public class ProfileFragment extends Fragment {
 
     private void handleCrop(int resultCode, Intent result) {
         if (resultCode == RESULT_OK) {
-            profileImage.setImageURI(Crop.getOutput(result));
+            filePath = Crop.getOutput(result);
+            profileImage.setImageURI(filePath);
+            uploadImage(filePath);
         } else if (resultCode == Crop.RESULT_ERROR) {
             Toast.makeText(getContext(), Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
         }
